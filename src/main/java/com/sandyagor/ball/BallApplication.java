d139f43ef06a4a4f2ab8327f54e9a1ff6c05a732
duzhou <duzhou@ccssoft.com.cn>
2018-11-02 21:44:46 +0800

    update .gitignore

---
+++ src/main/java/com/sandyagor/ball/BallApplication.java
@@ -0,0 +1,12 @@
+package com.sandyagor.ball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BallApplication.class, args);
+    }
+}
